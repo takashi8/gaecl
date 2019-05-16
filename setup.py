9f35d9bc7418ab14271027aad7d525fa50956e3f
moortz <takashi.mor@gmail.com>
2019-05-16 19:01:43 +0900

    Set same topic as google-cloud-logging

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Monitoring Tools',
+        'Topic :: Internet',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
